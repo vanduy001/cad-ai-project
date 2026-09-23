47e13ce23d914c7fd3bd6440c7b342ad1de1404c
vanduy001 <emtenduy01@gmail.com>
2026-09-23 22:22:21 +0700

    Add pillow_block

--- test_new.py
+++ test_new.py
@@ -1,6 +1,14 @@
+from spec_schema import PartSpec, Feature, validate_spec
 from cq_generator import generate_cadquery_code
-from spec_schema import example_plate_spec
 
-code = generate_cadquery_code(example_plate_spec())
-print(code)
-print("\n---OK khong loi---")
+spec = PartSpec(
+    part_type="pillow_block",
+    base_dimensions={
+        "length": 120, "depth": 78, "height": 42,
+        "base_height": 17, "top_width": 50, "seat_radius": 29,
+    },
+    features=[Feature(type="side_lugs", params={"lug_length": 12})],
+)
+
+print("Loi validate:", validate_spec(spec) or "khong co")
+print(generate_cadquery_code(spec))
